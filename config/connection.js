7c1a7cd03fc90df1c17564db1c172d830166e2e2
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2022-12-08 22:54:44 -0700

    comments updated

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,3 @@
-// sequelize constructor imported
 const Sequelize = require('sequelize')
 
 // .env file hides sql username and password. dotenv does not need to be saved to a variable
@@ -9,12 +8,12 @@ let sequelize
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
-  // The base sequelize class is being imported to create a new connection to the database
-  // Connection created to DATABASE
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // config settings being passed in
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   })
 }
+// The base sequelize class is being imported to create a new connection to the database
+// Connection created to DATABASE
 module.exports = sequelize
